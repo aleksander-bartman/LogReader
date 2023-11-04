41ff04ed9518469aeabf857df03d6035d3716a16
Olek <ba46466@zut.edu.pl>
2023-11-05 00:56:12 +0100

    update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,116 +1,158 @@
-#include <iostream>
+#include<iostream>
 #include<filesystem>
 #include<string>
 #include<vector>
-#include <algorithm>
-#include <fstream>
-#include<ctime>
+#include<algorithm>
+#include<fstream>
 #include<chrono>
-#include <map>
-#include <regex>
+#include<map>
+#include<regex>
+#include<time.h>
+#include<sstream>
 
 using namespace std;
 namespace fs = filesystem;
-/*
- Wymagania dotyczące działania aplikacji:
-1) po starcie szuka na dysku D: katalogu o nazwie 'logs',
-2) po znalezieniu katalogu, aplikacja plik po pliku interpretuje pliki z logami w kolejności lastModified descending,
-3) na koniec działania lub po każdym pliku aplikacja wylistowuje w konsoli:
-- czas jaki upłynął na czytanie pliku,
-- zakres logów w pliku (czyli różnica czasu miedzy pierwszym logiem w pliku a ostatnim),
-- ilość logów pogrupowana wg severity (np. INFO, WARN, ERROR),
-- stosunek ilości logów o severity ERROR lub wyższym do wszystkich logów,
-- ilość unikalnych wystąpień bibliotek w logu (ta wartość w kwadratowych nawiasach zaraz po oznaczeniu severity, np:
-[org.jboss.as.server]).
 
-Dodatkowo zaproponuj strukturę tabeli bazodanowej, która mogłaby posłużyć do przechowywania logów znajdujących
-się w pliku w bazie.
- */
 struct LogEntry {
-    string timestamp;
+    basic_string<char> timestamp;
     string severity;
     string message;
     string library;
 };
 
 map<string,int> severityCount;
+map<string,int> libraryCount;
 
-void ProcessLogEntry(const LogEntry& entry)
+void DisplayLogEntry(const LogEntry& entry)
 {
     cout<<"Timestamp: "<<entry.timestamp<<endl;
-    severityCount[entry.severity]++;
     cout<<"Severity: "<<entry.severity<<endl;
-    cout<<"Library: "<<entry.library<<endl;
-    cout<<"Message: "<<entry.message<<endl;
+    cout<<"Library: ["<<entry.library<<"]"<<endl;
     cout<<"--------------------------------------------------------"<<endl;
+}
+void CalculateTimeDiffrence(const vector<LogEntry>& logs)
+{
+    tm tm1 = {0};
+    istringstream ss1(logs.front().timestamp);
 
+    ss1 >> tm1.tm_year;
+    ss1.ignore();  // Ignorowanie znaku '-'
+    ss1 >> tm1.tm_mon;
+    ss1.ignore();
+    ss1 >> tm1.tm_mday;
+    ss1.ignore();
+    ss1 >> tm1.tm_hour;
+    ss1.ignore();
+    ss1 >> tm1.tm_min;
+    ss1.ignore();
+    ss1 >> tm1.tm_sec;
+
+    tm1.tm_year -= 1900;
+    tm1.tm_mon -= 1;
+
+    tm tm2 = {0};
+    istringstream ss2(logs.back().timestamp);
+
+    ss2 >> tm2.tm_year;
+    ss2.ignore();
+    ss2 >> tm2.tm_mon;
+    ss2.ignore();
+    ss2 >> tm2.tm_mday;
+    ss2.ignore();
+    ss2 >> tm2.tm_hour;
+    ss2.ignore();
+    ss2 >> tm2.tm_min;
+    ss2.ignore();
+    ss2 >> tm2.tm_sec;
+
+    tm2.tm_year -= 1900;
+    tm2.tm_mon -= 1;
+
+    time_t time1 = mktime(&tm1);
+    time_t time2 = mktime(&tm2);
+
+    int timeDifference = difftime(time2, time1);
+    int daysDifference = timeDifference / (60 * 60 * 24);
+
+    cout << "Roznica czasu miedzy pierwszym a ostatnim logiem: " << abs(daysDifference) << " dni" << endl;
 }
 int main() {
     string logsDirectory = "D:/logs";
-    vector<fs::path> logFiles;
-
-    for (const auto &entry : fs::directory_iterator(logsDirectory)) {
-        logFiles.push_back(entry.path());
-    }
-
-    sort(logFiles.begin(), logFiles.end(), [](const fs::path &a, const fs::path &b) {
-        return fs::last_write_time(a) > fs::last_write_time(b);
-    });
-
-    for (const auto &logFile : logFiles) {
-        cout << logFile << endl;
-        cout << endl;
-
-        chrono::steady_clock::time_point start_time = chrono::steady_clock::now();
-
-        ifstream file(logFile);
-        if (file.is_open()) {
-            LogEntry currentEntry;
-            string line;
-            bool inEntry = false;
-
-            while (getline(file, line)) {
-                //cout << line << endl;
-                if (!inEntry) {
-                    //regex timestampRegex("\\d{4}-\\d{2}-\\d{2} \\d{2}:\\d{2}:\\d{2},\\d{3}");
-                    regex timestampRegex("[0-9]{4}-(0[1-9]|1[0-2])-(0[1-9]|[1-2][0-9]|3[0-1]) (2[0-3]|[01][0-9]):[0-5][0-9]:[0-5][0-9],[0-9]{3}");
-                    if (regex_search(line, timestampRegex)) {
-                        currentEntry.timestamp = line.substr(0, 23);
-                        inEntry = true;
-                    }
-                } else {
-                    if (line.empty()) {
-                        ProcessLogEntry(currentEntry);
-                        currentEntry = LogEntry();
-                        inEntry = false;
-                    } else if (line.find("]") != string::npos) {
-                        size_t severityPos = line.find(" ",line.find(" ")+ 1);
-                        size_t libraryPos = line.find("]", severityPos) + 1;
-                        currentEntry.severity = line.substr(severityPos, libraryPos - severityPos);
-                        currentEntry.library = line.substr(libraryPos);
-                    } else {
-                        currentEntry.message += line + '\n';
-                    }
-                }
-            }
-            if (!currentEntry.timestamp.empty()) {
-                ProcessLogEntry(currentEntry);
-            }
-            file.close();
+    if (fs::exists(logsDirectory) && fs::is_directory(logsDirectory)) {
+        vector<fs::path> logFiles;
+        vector<LogEntry> logs;
+        for (const auto &entry: fs::directory_iterator(logsDirectory)) {
+            if (entry.is_regular_file() && entry.path().extension() == ".log")
+                logFiles.push_back(entry.path());
+        }
+        sort(logFiles.begin(), logFiles.end(), [](const fs::path &a, const fs::path &b) {
+            return fs::last_write_time(a) > fs::last_write_time(b);
+        });
+        if (logFiles.empty()) {
+            cout << "Brak plikow z logami" << endl;
         } else {
-            cout << "Nie można otworzyć pliku: " << logFile << endl;
-        }
-        chrono::steady_clock::time_point end_time = chrono::steady_clock::now();
-        chrono::milliseconds elapsed_time = chrono::duration_cast<chrono::milliseconds>(end_time - start_time);
+            for (const auto &logFile: logFiles) {
+                cout << logFile << endl;
+                chrono::steady_clock::time_point start_time = chrono::steady_clock::now();
+                logs.clear();
+                ifstream file(logFile);
+                if (file.is_open()) {
+                    string line;
+                    while (getline(file, line)) {
+                        LogEntry currentEntry;
+                        if (line.empty()) {
+                            currentEntry = LogEntry();
+                        } else {
+                            regex timestampRegex(
+                                    "[0-9]{4}-(0[1-9]|1[0-2])-(0[1-9]|[1-2][0-9]|3[0-1]) (2[0-3]|[01][0-9]):[0-5][0-9]:[0-5][0-9],[0-9]{3}");
+                            if (regex_search(line, timestampRegex)) {
+                                currentEntry.timestamp = line.substr(0, 19);
+                                if (line.find("[") != string::npos) {
+                                    size_t severityPosB = line.find(" ", line.find(" ") + 1);
+                                    size_t severityPosE = line.find(" ", severityPosB + 1);
+                                    size_t libraryPosB = line.find("[", severityPosE) + 1;
+                                    size_t libraryPosE = line.find("]", severityPosE);
+                                    currentEntry.severity = line.substr(severityPosB + 1,
+                                                                        severityPosE - severityPosB - 1);
+                                    currentEntry.library = line.substr(libraryPosB, libraryPosE - libraryPosB);
+                                    severityCount[currentEntry.severity]++;
+                                    libraryCount[currentEntry.library]++;
+                                    logs.push_back(currentEntry);
 
-        cout << "Czas odczytu pliku: " << elapsed_time.count() << " ms" << endl;
-        cout<<endl;
+                                }
+                            }
+                        }
+                    }
+                    file.close();
+                } else {
+                    cout << "Nie można otworzyć pliku: " << logFile << endl;
+                }
+                chrono::steady_clock::time_point end_time = chrono::steady_clock::now();
+                chrono::milliseconds elapsed_time = chrono::duration_cast<chrono::milliseconds>(end_time - start_time);
+                cout << "--------------------------------------------------------" << endl;
+                cout << "Czas odczytu pliku: " << elapsed_time.count() << " ms" << endl;
+                cout << endl;
+                CalculateTimeDiffrence(logs);
+                int count = 0;
+                int severityCountSize = 0;
+                cout << "\nStatystyki logow:" << endl;
+                for (const auto &severity: severityCount) {
+                    cout << severity.first << ": " << severity.second << " logow" << endl;
+                    if (severity.first == "ERROR" or severity.first == "FATAL")
+                        count++;
+                    severityCountSize += severity.second;
+                }
+                cout << "\nStosunek logow o severnity wyzszym badz rownym ERROR ";
+                cout << fixed << setprecision(2) << float(count) / float(severityCountSize) * 100 << "%\n" << endl;
+                cout << "Statystyki bibliotek:" << endl;
+                for (const auto &library: libraryCount)
+                    cout << library.first << ": " << library.second << endl;
+                cout << "--------------------------------------------------------\n" << endl;
+            }
+        }
+    }else
+    {
+        cout<<"Podany folder nie istnieje lub nie jest katalogiem"<<endl;
     }
-
-        std::cout << "Statystyki:" << std::endl;
-        for (const auto& severity : severityCount) {
-            std::cout << severity.first << ": " << severity.second << " logow" << endl;
-        }
-
     return 0;
 }
